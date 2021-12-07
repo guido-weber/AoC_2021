aaab098f55fc33ca52fb42b6993648f01094332e
Guido Weber <guido.weber0815@googlemail.com>
2021-12-07 16:44:02 +0100

    Solution template

---
+++ aoc_2021/template.py
@@ -0,0 +1,20 @@
+from aoc_2021 import common
+
+test_input = """
+"""
+
+
+def parse_input(lines: list[str]):
+    pass
+
+
+def part1(lines):
+    pass
+
+
+def part2(lines):
+    pass
+
+
+if __name__ == "__main__":
+    common.run(5, test_input, 42, test_input, 42, part1, part2)
